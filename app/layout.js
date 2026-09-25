5540092c1d4739948ba859263c6da89a74df2e2c
Uplakshya Kumar <uplakshyakumar.bt24ele@pec.edu.in>
2026-09-25 22:29:30 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -11,6 +11,7 @@ export default function RootLayout({ children }) {
     <html lang="en" className="h-full bg-slate-50">
       <body className="h-full flex flex-col font-sans text-slate-800 antialiased selection:bg-indigo-100 selection:text-indigo-800">
         {children}
+      <chatbot /> 
       </body>
     </html>
   );
